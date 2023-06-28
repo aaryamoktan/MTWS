f9a081e4ba47c390237358045009bba094e7318a
aarya moktan <aaryamoktan2055@gmail.com>
2023-06-28 08:58:09 -0700

    commit first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './App.scss'
+import Nevbar from './Content/Nevbar/Nevbar';
+import Home from './Content/Home/Home';
+import Features from './Content/Features/Features';
+import About from './Content/About/About';
+import Project from './Content/Projects/Project';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Nevbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/features" element={<Features/>}/>
+        <Route path="/project" element={<Project/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Content/Nevbar/Nevbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const img = require("../../images/logo.png")
+const Nevbar = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light ">
+  <Link class="navbar-brand" href="#">
+    <img src={img} alt="myogo"/>
+    <p>MT WEB SOLUTION</p>
+  </Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <Link class="nav-link" to="/">Home </Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link active" to="/features">Features</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link active" to="/project">Project</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link active" to="/about">About</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Nevbar
