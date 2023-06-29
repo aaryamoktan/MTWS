a06752a1d778ef2d829eca801e5ff6a9e17035b1
aarya moktan <aaryamoktan2055@gmail.com>
2023-06-28 20:54:58 -0700

    commit first

--- src/Content/Home/Home.js
+++ src/Content/Home/Home.js
@@ -2,13 +2,25 @@ import React from 'react'
 const img = require("../../images/logo.png")
 const Home = () => {
   return (
-    <div>
-    <img src={img} alt="mylogo"/>
-    <br/> <img src={img} alt="mylogo"/>
-    <br/> <img src={img} alt="mylogo"/>
-    
-      
-    </div>
+    <>
+      <div className='Homecontainer'>
+        <div className='homeinnercontainer'>
+          <div className='Logo'>
+            <img src={img} alt="logo"/>
+          </div>
+          <div className='logoinfo'>
+            <div className='logoinfoinner'>
+              <p>MT Web solution is the one of the company in nepal made by all it student . The main purpose of our comapany to degitilized the Nepal.
+              Our main goal to make  all Nepal Online even for rular areas.
+              In context of Nepal many of school and college or government sector they dont have website.
+              They used facebook page is main medium to shere information to ther student and worker associate with respective organization.
+              So, To overcome these problem MT WEB SOLUTION plays vilal role.
+              We developed website according to your requirement.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
