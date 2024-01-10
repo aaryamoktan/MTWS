96e103701fe77ab548ad73596f1944e4ec40413f
aarya moktan <aaryamoktan2055@gmail.com>
2024-01-10 09:45:05 -0800

    first commit

--- src/Content/Footer/Footer.js
+++ src/Content/Footer/Footer.js
@@ -19,12 +19,12 @@ const Footer = () => {
             </div>
             <div className='footercontaint3'>
             <h1>Social-Media</h1>
-            <a href="https://www.facebook.com/profile.php?id=61555124925505&mibextid=eHce3h" target="blank"><i class="fa-brands fa-square-facebook">{<ImFacebook2/>}</i></a>
+            <a href="https://www.facebook.com/profile.php?id=61555124925505&mibextid=eHce3h" target="blank"><i class="fa-brands fa-square-facebook">facebook</i></a>
             <br/>
-            <a href="/" target="blank"><i class="fa-brands fa-square-instagram" style={{}}><BsInstagram/>
+            <a href="/" target="blank"><i class="fa-brands fa-square-instagram" style={{}}>instagram
             </i></a>
             <br/>
-            <a href="/" target="blank"><i class="fa-brands fa-square-twitter"></i><FaSquareTwitter/></a>
+            <a href="/" target="blank"><i class="fa-brands fa-square-twitter"></i>twitter</a>
             </div>
         </div>
     )
